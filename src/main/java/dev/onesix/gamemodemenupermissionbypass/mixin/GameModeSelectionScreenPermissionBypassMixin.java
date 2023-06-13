2d568901968a1e536c4147d5b69edb19f6d033aa
0b10000 <zerob10000@tutanota.com>
2023-06-13 15:57:57 -0400

    Update to 1.20

--- src/main/java/dev/onesix/gamemodemenupermissionbypass/mixin/GameModeSelectionScreenPermissionBypassMixin.java
+++ src/main/java/dev/onesix/gamemodemenupermissionbypass/mixin/GameModeSelectionScreenPermissionBypassMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameModeSelectionScreen.class)
 public class GameModeSelectionScreenPermissionBypassMixin {
-    @Redirect(method = "apply(Lnet/minecraft/client/MinecraftClient;Ljava/util/Optional;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z"))
+    @Redirect(method = "apply(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/GameModeSelectionScreen$GameModeSelection;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z"))
     private static boolean hasPermissionLevel(ClientPlayerEntity entity, int permissionLevel){
         return true;
     }
